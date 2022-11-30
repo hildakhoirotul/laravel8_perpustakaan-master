9f3bb72d7e821a6b66490cf2db41da30c9586b5f
Chan-Paul-Amol <99714187+Chan-Paul-Amol@users.noreply.github.com>
2022-11-30 08:55:06 +0700

    Category Test Cases

--- cypress/e2e/Category.cy.js
+++ cypress/e2e/Category.cy.js
@@ -1,5 +1,52 @@
-describe('empty spec', () => {
+// User Add a category
+describe('User Add a Category', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://127.0.0.1:8000/login');
+    cy.get('#email').type('admin@gmail.com');
+    cy.get('#password').type('123123123');
+    cy.get('form').submit();
+    cy.get('[href="#"] > .nav-icon').click()
+    cy.get('[href="#"] > .nav-icon').click()
+    cy.get(':nth-child(1) > .nav-link > .far').click()
+    cy.get('#tambah').click();
+    cy.get('#nama').type('new_category');
+    cy.get('#sim').click();
+    cy.get('.ml-auto > .nav-item > .nav-link').click()
+  })
+})
+
+//  Use Edit A Category
+describe('User Edit a Category', ()=>{
+  it('passes', () =>{
+    cy.visit('http://127.0.0.1:8000/');
+    cy.get('.ml-auto > :nth-child(1) > .nav-link').click();
+    cy.get('#email').type('admin@gmail.com');
+    cy.get('#password').type('123123123');
+    cy.get('form').submit();
+    cy.get('[href="#"] > .nav-icon').click();
+    cy.get('[href="#"] > .nav-icon').click();
+    cy.get(':nth-child(1) > .nav-link > .far').click();
+    cy.get('#edit').click();
+     cy.get('#nama').clear();
+    cy.get('#nama').type('Novel');
+    cy.get('#update').click();
+    cy.get('.ml-auto > .nav-item > .nav-link').click();
+  })
+})
+
+// User Delete a category
+describe('User Delete a Category', ()=>{
+  it('passes', () =>{
+    cy.visit('http://127.0.0.1:8000/');
+    cy.get('.ml-auto > :nth-child(1) > .nav-link').click();
+    cy.get('#email').type('admin@gmail.com');
+    cy.get('#password').type('123123123');
+    cy.get('form').submit();
+    cy.get('[href="#"] > .nav-icon').click();
+    cy.get('[href="#"] > .nav-icon').click();
+    cy.get(':nth-child(1) > .nav-link > .far').click();
+    cy.get('#delete').click();
+    cy.get('#hapus').click();
+    cy.get('.ml-auto > .nav-item > .nav-link').click();
   })
 })
--- resources/views/livewire/petugas/kategori.blade.php
+++ resources/views/livewire/petugas/kategori.blade.php
@@ -1,66 +1,68 @@
 <div class="row">
     <div class="col-12">
 
-    @include('admin-lte/flash')
+        @include('admin-lte/flash')
 
-    @include('petugas/kategori/create')
-    @include('petugas/kategori/edit')
-    @include('petugas/kategori/delete')
+        @include('petugas/kategori/create')
+        @include('petugas/kategori/edit')
+        @include('petugas/kategori/delete')
 
-    <div class="card">
-        <div class="card-header">
-        <span wire:click="create" class="btn btn-sm btn-primary">Tambah</span>
+        <div class="card">
+            <div class="card-header">
+                <span wire:click="create" class="btn btn-sm btn-primary" id="tambah">Tambah</span>
 
-      
-              <div class="card-tools">
-                <div class="input-group input-group-sm" style="width: 150px;">
-                <input wire:model="search" type="text" name="table_search" class="form-control float-right" placeholder="Search">
+                <div class="card-tools">
+                    <div class="input-group input-group-sm" style="width: 150px;">
+                        <input wire:model="search" type="text" name="table_search" class="form-control float-right"
+                            placeholder="Search">
 
-                <div class="input-group-append">
-                    <button type="submit" class="btn btn-default">
-                    <i class="fas fa-search"></i>
-                    </button>
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-default">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
             </div>
             <!-- /.card-header -->
             @if ($kategori->isNotEmpty())
-                <div class="card-body table-responsive p-0">
-                    <table class="table table-hover text-nowrap">
-                        <thead>
+            <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                    <thead>
                         <tr>
                             <th width="10%">No</th>
                             <th>Kategori</th>
                             <th width="15%">Aksi</th>
                         </tr>
-                        </thead>
-                        <tbody>
+                    </thead>
+                    <tbody>
                         @foreach ($kategori as $item)
-                            <tr>
-                                <td>{{$loop->iteration}}</td>
-                                <td>{{$item->nama}}</td>
-                                <td>
-                                    <div class="btn-group">
-                                        <span wire:click="edit({{$item->id}})" class="btn btn-sm btn-primary mr-2">Edit</span>
-                                        <span wire:click="delete({{$item ->id}})" class="btn btn-sm btn-danger">Hapus</span>
-                                    </div>
-                                </td>
-                            </tr>
+                        <tr>
+                            <td>{{$loop->iteration}}</td>
+                            <td>{{$item->nama}}</td>
+                            <td>
+                                <div class="btn-group">
+                                    <span wire:click="edit({{$item->id}})" class="btn btn-sm btn-primary mr-2"
+                                        id="edit">Edit</span>
+                                    <span wire:click="delete({{$item ->id}})" class="btn btn-sm btn-danger"
+                                        id="delete">Hapus</span>
+                                </div>
+                            </td>
+                        </tr>
                         @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                <!-- /.card-body -->
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
             @endif
-    </div>
-    <!-- /.card -->
+        </div>
+        <!-- /.card -->
 
-    <div class="row justify-content-center">
-        {{$kategori->links()}}
-    </div>
+        <div class="row justify-content-center">
+            {{$kategori->links()}}
+        </div>
 
-    @if ($kategori->isEmpty())
+        @if ($kategori->isEmpty())
         <div class="card">
             <div class="card-body">
                 <div class="alert alert-warning">
@@ -68,7 +70,7 @@
                 </div>
             </div>
         </div>
-    @endif
+        @endif
 
     </div>
 </div>
